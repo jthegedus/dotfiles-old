ad2fec0a26282e6eae4294b5a4216e14781f1f11
jthegedus <jthegedus@hey.com>
2023-11-12 03:03:55 +1100

    fix: create dirs for symlinks

--- setup.bash
+++ setup.bash
@@ -4,5 +4,6 @@ REPO_ROOT="$(dirname "$(realpath "$0")")"
 
 find "$REPO_ROOT/home" -type f | while read -r file; do
 	target="$HOME/${file#"$REPO_ROOT"/home/}"
+	mkdir -p "$(dirname "$target")"
 	ln --symbolic --force --verbose "$file" "$target"
 done
